e9a618a3244dd42bf8c7d75f9d5148188c4d16a3
Edgars Skujins <edgars.skujins@ms-idi.eu>
2021-02-19 11:45:15 +0200

    Added lv translation

---
+++ wordcount/lang/lv.js
@@ -0,0 +1,16 @@
+/*
+Latvian translation by Edgars Skujins
+*/
+CKEDITOR.plugins.setLang('wordcount', 'lv', {
+    WordCount: 'Vārdu:',
+    WordCountRemaining: 'Vārdu atlicis',
+    CharCount: 'Simbolu:',
+    CharCountRemaining: 'Simbolu atlicis',
+    CharCountWithHTML: 'Simbolu (ieskaitot HTML):',
+    CharCountWithHTMLRemaining: 'Simbolu (ieskaitot HTML) atlicis',
+    Paragraphs: 'Rindkopas:',
+    ParagraphsRemaining: 'Rindkopu atlicis',
+    pasteWarning: 'Saturu nevar ielīmēt, jo tas pārsniedz atļauto ierobežojumu',
+    Selected: 'Atlasīts: ',
+    title: 'Statistika'
+});
